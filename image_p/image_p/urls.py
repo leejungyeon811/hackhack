da9b9b7b50b3cb4093a3c407a81c1cb83bc23b2f
angel <angel0308dl@naver.com>
2024-06-28 10:31:02 +0900

    실행시 main이 뜨도록 수정

--- image_p/image_p/urls.py
+++ image_p/image_p/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from core import views #수정함
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+     path('', views.main_page, name='main_page'),  # 기본 페이지를 main_page로 설정
 ]
